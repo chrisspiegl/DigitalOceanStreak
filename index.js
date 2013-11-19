6adb40e824bc6f0ed54145a333c3e2839ffb5f79
Christoph Spiegl <chris@chrissp.com>
2013-11-19 20:57:00 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-if (process.env.DOS_DEV) {
+
+if (process.env.DOS_COV) {
+  console.log('Running COVERAGE');
+  require('coffee-script');
+  require('coffee-cache').setCacheDir('tmp/cachedCoffee/');
+  module.exports = require('./lib-cov');
+
+} else if (process.env.DOS_DEV) {
+  console.log('Running DEVELOPMENT');
+  process.env.NODSTR_DEV = true;
+  require('coffee-script');
+  require('coffee-backtrace').setContext(5);
+  require('coffee-cache').setCacheDir('tmp/cachedCoffee/');
+  require('source-map-support')
   module.exports = require('./lib-coffee');
+
 } else {
+  console.log('Running PRODUCTION');
   module.exports = require('./lib');
+
 }
